1e23dfc7156050c66ce7baffd15f3300d4a7fddc
San Juan, Edson Ray <qeresanjuan@tip.edu.ph>
2024-09-21 20:55:26 +0800

    Add files via upload

---
+++ OOPIntro_SANJUAN/Accounts.py
@@ -0,0 +1,21 @@
+"""
+        Accounts.py
+"""
+
+class Accounts():
+    def __init__(self, account_number, account_firstname, account_lastname, 
+                 current_balance, address, email): # Constructor class
+        self.account_number = account_number
+        self.account_firstname = account_firstname
+        self.account_lastname = account_lastname
+        self.current_balance = current_balance
+        self.address = address
+        self.email = email
+    
+    def update_address(self, new_address):
+        self.address = new_address
+        
+    def update_email(self, new_email):
+        self.email = new_email
+        
+        
